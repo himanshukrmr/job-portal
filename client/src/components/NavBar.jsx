ea0a0269edcf38ed63f646aaa091db82ea12ca9c
himanshukrmr <himanshukrmr@gmail.com>
2025-02-10 20:17:38 +0530

    added the Hero component and styled it

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,20 +10,20 @@ const NavBar = () => {
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
-        <img src={assets.logo} alt="" />
+        <img className="cursor-pointer" src={assets.logo} alt="" />
         {user ? (
-          <div className="flex gap-3 items-center"> 
+          <div className="flex gap-3 items-center">
             <Link to={"/applications"}>Applied jobs</Link>
             <p>|</p>
             <p>Hi, {user.firstName + " " + user.lastName}</p>
-            <UserButton/>
+            <UserButton />
           </div>
         ) : (
           <div className="flex gap-4">
-            <button className="text-gray-600">Recuriter Login</button>
+            <button className="text-gray-600 cursor-pointer">Recuriter Login</button>
             <button
               onClick={(e) => openSignIn()}
-              className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
+              className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full cursor-pointer"
             >
               Login
             </button>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
-    <div>
+    <>
       <NavBar/>
-    </div>
+      <Hero/>
+    </>
   )
 }
 
