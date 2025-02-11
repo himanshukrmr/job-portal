1f3b60c93e9311e6ce267b772839ab0faf009c6d
himanshukrmr <himanshukrmr@gmail.com>
2025-02-11 13:23:25 +0530

    added the search functionality in the hero component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
 import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 
 const Hero = () => {
+
+    const{setIsSearched, setSearchFilter} = useContext(AppContext);
+
+    const titleRef = useRef(null);;
+    const locationRef = useRef(null);
+
+    const searchJobs = () => { 
+        setSearchFilter({
+            title: titleRef.current.value,
+            location: locationRef.current.value
+        });
+        setIsSearched(true);                                                                                                                                                                                                                     
+    }
+
   return (
     <div className="container 2xl:px-20 mx-auto my-10">
       <div className="bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl">
@@ -23,6 +38,7 @@ const Hero = () => {
               type="text"
               placeholder="Search for jobs"
               className="max-sm:text-xs p-2 rounded outline-none w-full"
+              ref={titleRef}
             />
           </div>
           <div className="flex items-center">
@@ -35,9 +51,10 @@ const Hero = () => {
               type="text"
               placeholder="Location"
               className="max-sm:text-xs p-2 rounded outline-none w-full"
+              ref={locationRef}
             />
           </div>
-          <button className="bg-blue-600 px-6 py-2 rounded text-white m-1 cursor-pointer">
+          <button onClick={searchJobs} className="bg-blue-600 px-6 py-2 rounded text-white m-1 cursor-pointer">
             Search
           </button>
         </div>
